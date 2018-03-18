eaa096991ff3e494db7d6c20da0b401c97dd3030
Mathieu Marques <mathieumarques78@gmail.com>
2018-03-18 19:42:33 +0100

    Implement a simple double cantrip probability
    It does not take mana costs into account yet.

--- probability.py
+++ probability.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from binomial import binomial
+from binomial import binomial as b
+from starsnbars import starsnbars
 
 
 """
 The following functions expect a well-formed deck as implemented in deck.Deck.
+
+Prelude:
+
+  We are assuming cards are castable either by themselves for no mana (eg.
+  Gitaxian Probe) or from a land and/or Lotus Petal.
 
 Notation:
 
@@ -19,12 +25,17 @@ Notation:
   BZ -- Pile business
         This can be any of the elligible business pieces to put back with a
         Brainstorm.
+  Z  -- Unused card
 
 Functions:
 
+  These functions basically divide the count of plausible hands by the count of
+  total hands. Furthermore, each function implements a customized handling of
+  mana costs.
+
   DOUBLE_CANTRIP
-  A double cantrip pile needs the following starting hand:
-  DR, DD, C, C
+  A double cantrip hand consists of 2 cantrips, usually to consume a pile
+  starting with LED and GP.
 """
 
 
@@ -42,9 +53,21 @@ class Probability:
         for it in self.deck.library:
             for flag in flags:
                 self.data[flag] += 1 if flag in it['flags'] else 0
+        self.total = b(60, 7)
 
     def double_cantrip(self):
-        """Return the probability of a double cantrip pile.
+        """Return the probability of a double cantrip hand.
+
+        A double cantrip hand looks like [DR, DD, C, C, Z, Z, Z].
         """
 
-        return binomial(3, 1) * binomial(4, 1) * binomial(16, 2) * binomial(60, 3) / binomial(60, 7)
+        count_c = self.data['C']
+        count_dd = self.data['DD']
+        count_dr = self.data['DR']
+        count_z = len(self.deck.library) - count_c - count_dd - count_dr
+        return (
+            sum((
+                b(count_c, 2 + C) * b(count_dd, 1 + DD) * b(count_dr, 1 + DR) * b(count_z, Z)
+                for DR, DD, C, Z in starsnbars(3, 3)
+            )) / self.total
+        )
