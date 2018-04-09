a70cb44d30949ddce989e3d37b960b9efc1b4f1d
Mathieu Marques <mathieumarques78@gmail.com>
2018-04-09 17:14:57 +0200

    Read deck file from command line arguments

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+
 from deck import Deck
 from probability import Probability
 
 
 if __name__ == '__main__':
-    deck = Deck(file='')
+    parser = ArgumentParser()
+    parser.add_argument('deck', help='provide a deck file')
+    arguments = parser.parse_args()
+    deck = Deck(file=arguments.deck, verbose=arguments.verbose)
     probability = Probability(deck=deck)
     probability.run()
