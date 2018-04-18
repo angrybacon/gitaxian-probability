a9cb6052ac27a1bef38c5fc4740d05f3a97a3af5
Mathieu Marques <mathieumarques78@gmail.com>
2018-04-18 16:26:01 +0200

    Clean up

--- slate-doomsday.py
+++ slate-doomsday.py
@@ -7,16 +7,7 @@
 The SLATE table contains the corresponding flags to each card that is relevant
 to Doomsday as an archetype.
 
-Why?
-
-  This is useful to count a given "kind" of cards accross the library.
-
-Notation:
-
-  See the more common abbreviations at
-  http://ddft.wiki/pages-output/ch1/basics/.
-
-The FORMS table lists all forms for a first turn kill
+The FORMS table lists all forms for a first turn kill.
 """
 
 
@@ -24,10 +15,10 @@ SLATE = {
 
     # Business
     'Act on Impulse':       ('AoI',),
-    'Burning Wish':         ('BW',),
+    # 'Burning Wish':         ('BW',),
     'Doomsday':             ('DD',),
     'Ideas Unbound':        ('IU',),
-    'Laboratory Maniac':    (),
+    # 'Laboratory Maniac':    ('LM',),
     'Lion\'s Eye Diamond':  ('LED',),
     'Three Wishes':         ('TW',),
 
@@ -41,15 +32,19 @@ SLATE = {
 
     # Mana
     'Badlands':             ('L', 'B', 'R',),
+    # 'Cabal Ritual':         ('CR',),
     'Dark Ritual':          ('DR',),
     'Island':               ('L', 'U',),
     'Lotus Petal':          ('LP',),
-    'Polluted Delta':       ('L', 'U', 'B', 'R', 'G',),
-    'Scalding Tarn':        ('L', 'U', 'B', 'R', 'G',),
+    'Marsh Flats':          ('L', 'W', 'U', 'B', 'R', 'G',),
+    'Plains':               ('L', 'W',),
+    'Polluted Delta':       ('L', 'W', 'U', 'B', 'R', 'G',),
+    'Scalding Tarn':        ('L', 'W', 'U', 'B', 'R', 'G',),
     'Swamp':                ('L', 'B',),
     'Tropical Island':      ('L', 'U', 'G',),
+    'Tundra':               ('L', 'W', 'U',),
     'Underground Sea':      ('L', 'U', 'B',),
-    'Verdant Catacombs':    ('L', 'U', 'B', 'R', 'G',),
+    'Verdant Catacombs':    ('L', 'W', 'U', 'B', 'R', 'G',),
     'Volcanic Island':      ('L', 'U', 'R',),
 }
 
@@ -95,7 +90,7 @@ FORMS = {
         ((2, 'DR'),      (1, 'L|LP'),                    (1, 'IU'),      (1, 'CS'),                      ),  # 6
     ],
 
-    'LED in hand': [
+    'LED in Hand': [
         {'base': ((1, 'DD'),)},
         ((1, 'DR'),      (1, 'L|LP'),                    (1, 'LED'),     (1, 'GP'),                      ),  # 5
         ((1, 'DR'),      (1, '=L'),      (1, '=LP'),     (1, 'LED'),     (1, '=C'),      (0, 'GP'),      ),  # 6
