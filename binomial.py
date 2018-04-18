b821912d2c136c7a367bba9012fa9460a593b24e
Mathieu Marques <mathieumarques78@gmail.com>
2018-04-18 11:04:05 +0200

    Add support for binomial.verbose

--- binomial.py
+++ binomial.py
@@ -5,7 +5,7 @@
 from math import factorial
 
 
-def binomial(x, y):
+def binomial(x, y, verbose=False):
     """Return  the binomial coefficient for x chooses y.
     """
 
@@ -13,4 +13,6 @@ def binomial(x, y):
         result = factorial(x) // factorial(y) // factorial(x - y)
     except ValueError:
         result = 0
+    if verbose is True:
+        print('b({}, {}) = {}'.format(x, y, result))
     return result
