4ea13f3452481db01ff3d5f04cde69ea593166d5
Mathieu Marques <mathieumarques78@gmail.com>
2019-06-19 15:46:50 +0200

    Add Mono Black Control slate

---
+++ slate-mbc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+
+SLATE = {
+    'Marsh Flats':        ('L',),
+    'Polluted Delta':     ('L',),
+    'Swamp':              ('L', 'S',),
+    'Tarnished Citadel':  ('L',),
+}
+
+FORMS = {
+    '7 lands': [
+        ((7, 'L'),),
+    ],
+    '7 copies of Swamp': [
+        ((7, 'S'),),
+    ],
+}
